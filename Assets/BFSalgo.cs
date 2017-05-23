5595d5977aee9e64b168a4e76d71bbdb7ddfa2eb
dadaday <aaurub@gmail.com>
2017-05-24 00:20:30 +0600

    GUI, TODO: complete BFS :calendar:

---
+++ Assets/BFSalgo.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BFSalgo {
+	private int NIL;
+	private Dictionary<int, List<int>> graph;
+	private int NUM;
+	private int DIM;
+
+	private enum STATE {
+		NOTVISITED,
+		VISITED,
+		FINISHED
+	};
+
+	public BFSalgo(int num) {
+		NIL = -1;
+		NUM = num;
+		DIM = num * num;
+		graph = new Dictionary<int, List<int>> ();
+
+		for (int row = 0; row < NUM; row++) {
+			for (int col = 0; col < NUM; col++) {
+				List<int> temp = new List<int> ();
+				if (row < (NUM-1)) {
+					temp.Add ((row+1) * NUM + col);	
+				}
+				if (row > 0) {
+					temp.Add ((row-1) * NUM + col);	
+				}
+				if (col > 0) {
+					temp.Add (row * NUM + (col-1));	
+				}
+				if (col < (NUM-1)) {
+					temp.Add (row * NUM + (col+1));	
+				}
+
+				graph.Add ((row * NUM + col), temp);
+			}
+		}
+	}
+
+	public void addTies(int row, int col) {
+		int coord = (row * NUM + col);
+		List<int> temp = new List<int> ();
+
+		if (row < (NUM-1)) {
+			temp.Add ((row+1) * NUM + col);
+			graph [(row + 1) * NUM + col].Add (coord);
+		}
+		if (row > 0) {
+			temp.Add ((row-1) * NUM + col);	
+			graph [(row - 1) * NUM + col].Add (coord);
+		}
+		if (col > 0) {
+			temp.Add (row * NUM + (col-1));	
+			graph [row * NUM + (col-1)].Add (coord);
+		}
+		if (col < (NUM-1)) {
+			temp.Add (row * NUM + (col+1));	
+			graph [row * NUM + (col+1)].Add (coord);
+		}
+
+		if(temp.Count == 4)
+			Debug.Log ((row * NUM + col) + " is connected to " + temp[0] +  " " + temp[1] +  " " + temp[2] +  " " + temp[3]);
+		else if(temp.Count == 3)
+			Debug.Log ((row * NUM + col) + " is connected to " + temp[0] +  " " + temp[1] +  " " + temp[2]);
+		else if(temp.Count == 2)
+			Debug.Log ((row * NUM + col) + " is connected to " + temp[0] +  " " + temp[1]);
+
+		graph [row * NUM + col].Clear();
+		graph [row * NUM + col] = temp;
+	}
+
+	public void cutTies(int row, int col) {
+		int coord = (row * NUM + col);
+		Debug.Log ("CUTTING TIES FOR " + row + " " + col + " " + coord);
+
+		if (row < (NUM-1)) {
+			graph [(row + 1) * NUM + col].Remove(coord);
+			Debug.Log ("CANT COME TO " + coord + "FROM " + ((row + 1) * NUM + col));
+		}
+		if (row > 0) {
+			graph [(row - 1) * NUM + col].Remove(coord);
+			Debug.Log ("CANT COME TO " + coord + "FROM " + ((row - 1) * NUM + col));
+		}
+		if (col > 0) {
+			graph [row * NUM + (col-1)].Remove(coord);
+			Debug.Log ("CANT COME TO " + coord + "FROM " + (row * NUM + (col-1)));
+		}
+		if (col < (NUM-1)) {
+			graph [row * NUM + (col+1)].Remove(coord);
+			Debug.Log ("CANT COME TO " + coord + "FROM " + (row * NUM + (col+1)));
+		}
+	}
+
+	public bool checkAndPutBall(int sourceRow, int sourceCol, int destRow, int destCol) {
+		Queue<int> burned = new Queue<int>();
+		STATE[] states = new STATE[DIM];
+		int[] predec = new int[DIM];
+
+		for (int i = 0; i < DIM; i++) {
+			states [i] = STATE.NOTVISITED;
+			predec [i] = NIL;
+		}
+
+		int sourceCoord = sourceRow * NUM + sourceCol;
+		int destCoord = destRow * NUM + destCol;
+
+		Debug.Log ("Looking for path from r: " + sourceRow + " c: " + sourceCol + " to r: " + destRow + " c: " + destCol);
+		Debug.Log ("sourceCoord: " + sourceCoord + " destCoord: " + destCoord);
+
+		burned.Enqueue (sourceCoord);
+		states [sourceCoord] = STATE.VISITED;
+
+		while (burned.Count > 0) {
+			int u = burned.Dequeue ();
+			states [u] = STATE.FINISHED;
+
+			foreach (int it in graph[u]) {
+				if (states [it] == STATE.NOTVISITED) {
+					states[it] = STATE.VISITED;
+					predec[it] = u;
+					burned.Enqueue(it);
+				}
+			}
+		}
+
+		if (predec [destCoord] != NIL) {
+			List<int> path = new List<int> ();
+			int dest = destCoord;
+			path.Add (dest);
+
+			while (dest != sourceCoord) {
+				dest = predec [dest];
+				path.Add (dest);
+			}
+
+			path.Reverse ();
+
+			Debug.Log ("Path is: ");
+			foreach (int p in path) {
+				Debug.Log (p);
+			}
+
+			cutTies (sourceRow, sourceCol);
+			addTies (sourceRow, sourceCol);	
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ Assets/MainScript.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScript : MonoBehaviour {
+	BFSalgo bAlgo;
+
+	public static int NUM = 9;
+	public static int BPSTEP = 3;
+
+	private const int COLORSNUMBER = 7;
+	private int BOARDDIM;
+
+	public GameObject cellPrefab;
+	public GameObject ballPrefab;
+
+	private Color[,] board;
+	private GameObject[] balls;
+
+	private float cellWidth = 1.0f;
+	private float ballSize = 0.8f;
+	private Color DARKRED;
+	private Color[] ballColors;
+
+	private bool finished;
+	private int ballsOnTable;
+
+	private Vector3 clickedCoord;
+	private bool ballSelected;
+
+	// Use this for initialization
+	void Start () {
+		BOARDDIM = NUM * NUM;
+		bAlgo = new BFSalgo(NUM);
+
+		board = new Color[NUM, NUM];
+		balls = new GameObject[BOARDDIM];
+
+		DARKRED = new Color (150 / 255.0f, 17 / 255.0f, 17 / 255.0f);
+		ballColors = new Color[COLORSNUMBER] {Color.red, Color.green, Color.blue, Color.yellow, Color.cyan, Color.magenta, DARKRED};
+
+		finished = false;
+		ballsOnTable = 0;
+		clickedCoord = new Vector3 (-1, -1, -1);
+		ballSelected = false;
+
+		drawTable (NUM);	
+
+		for (int i = 0; i < NUM; i++) {
+			for (int j = 0; j < NUM; j++) {
+				board [i,j] = Color.black;
+			}
+		}
+
+		generateBalls (BPSTEP);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!finished) {
+			if (Input.GetMouseButtonDown(0))
+			{
+				RaycastHit hitInfo = new RaycastHit();
+				bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+				if (hit) 
+				{
+					GameObject selectedGO = hitInfo.transform.gameObject;
+			
+					if (selectedGO.tag == "Ball")
+					{
+						ballSelected = true;
+						clickedCoord = selectedGO.GetComponent<Transform> ().position;
+						int clickedRow = (int) clickedCoord.x;
+						int clickedCol = (int) clickedCoord.z;
+		
+						Debug.Log ("Ball at row: " + clickedRow + " col: " + clickedCol + " is clicked: " + (clickedRow * NUM + clickedCol));
+
+					} else if (ballSelected && selectedGO.tag == "Cell") {
+						int clickedRow = (int) clickedCoord.z;
+						int clickedCol = (int) clickedCoord.x;
+
+						Vector3 moveTo = selectedGO.GetComponent<Transform> ().position;
+						int moveToRow = (int) moveTo.x;
+						int moveToCol = (int) moveTo.z;
+
+						Debug.Log ("Wanna move the ball at row: " + clickedRow + " col: " + clickedCol + " to cell row: " + moveToRow + " col: " + moveToCol);
+						moveBall (clickedCoord, moveTo);
+
+						ballSelected = false;
+					}
+				} else {
+					Debug.Log("No hit");
+				}
+			} 
+
+			if (Input.GetKeyDown ("space")) {
+				generateBalls (BPSTEP);
+			}
+		}
+	}
+
+	private void generateBalls(int numBalls) {
+		int added = 0;
+		while (added < numBalls && !finished) {
+			int row = Random.Range (0, NUM);
+			int col = Random.Range (0, NUM);
+			int color = Random.Range (0, COLORSNUMBER);
+
+			if (putBall(row, col, ballColors[color])) {
+				added++;
+				ballsOnTable++;
+				if (ballsOnTable >= BOARDDIM) {
+					Debug.Log ("MAX");
+					finished = true;
+				}
+			}
+		}
+	}
+
+	// put the ball of given color at given row and column
+	private bool putBall(int row, int col, Color color) {
+		if (board [row, col] == Color.black) {
+			bAlgo.cutTies (row, col);
+
+			board [row, col] = color;
+			balls [row * NUM + col] = (GameObject)Instantiate (ballPrefab, new Vector3 (row * cellWidth, ballSize / 2.0f, col * cellWidth), Quaternion.identity);
+			balls [row * NUM + col].GetComponent<MeshRenderer> ().material.SetColor ("_Color", color);
+			Debug.Log ("putBall at: " + (row * NUM + col));
+			return true;
+		}
+		return false;
+	}
+
+	private void moveBall(Vector3 source, Vector3 dest) {
+		int sourceCol = (int) source.z;
+		int sourceRow = (int) source.x;
+
+		int destCol = (int) dest.z;
+		int destRow = (int) dest.x;
+
+		if (board [destRow, destCol] == Color.black
+			&& bAlgo.checkAndPutBall(sourceRow, sourceCol, destRow, destCol)) {
+
+			board [destRow, destCol] = board [sourceRow, sourceCol];
+			board [sourceRow, sourceCol] = Color.black;
+
+			balls [destRow * NUM + destCol] = balls [sourceRow * NUM + sourceCol];
+			balls[destRow * NUM + destCol].GetComponent<Transform> ().position = new Vector3(dest.x, source.y, dest.z);
+		} else {
+			Debug.Log ("CANT PUT AT ROW: " + destRow + " COL: " + destCol);
+		}
+	}
+
+	private void putBalls(Color color) {
+		for (int i = 0; i < NUM; i++) {
+			for (int j = 0; j < NUM; j++) {
+					putBall(i, j, color);
+			}
+		}
+	}
+
+	private void drawTable(int num) {
+
+		cellPrefab.transform.localScale = new Vector3(cellWidth, 0.1f, cellWidth);
+
+		for (int row = 0; row < num; ++row) {
+			for (int col = 0; col < num; ++col) {
+				Instantiate (cellPrefab, new Vector3 (row * cellWidth, 0, col * cellWidth), Quaternion.identity);
+			}
+		}
+	}
+
+	public void ExitApplication() {
+		Application.Quit ();
+	}
+}
